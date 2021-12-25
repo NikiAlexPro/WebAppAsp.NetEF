6ceec6c05edd0b4b1ce7d4a64cd6cd9a5f02cc91
Nik <NikiAlexPro@gmail.com>
2021-12-26 01:14:40 +0300

    Modal window

---
+++ WebAppCustom/Models/InfoClient.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+namespace WebAppCustom.Models
+{
+    public class InfoClient
+    {
+        public int ID { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Client_id { get; set; }
+
+        [ForeignKey("Client_id")]
+        public Client Client { get; set; }
+    }
+}
--- WebAppCustom/Pages/Customers.cshtml.cs
+++ WebAppCustom/Pages/Customers.cshtml.cs
@@ -8,40 +8,31 @@ namespace WebAppCustom.Pages
     public class CustomersModel : PageModel
     {
 
-        public List<Client> listClients { get; set; }
+        public List<Client>? listClients { get; set; }
         //public List<InfoClient> infoClients { get; set; }
-
-        //public List<(Client, InfoClient)> clients { get; set; }
+        public List<ListShop>? shops { get; set; }
         
+        //public List<FullClient> fullClients { get; set; }
 
         public void OnGet()
         {
             using (ApplicationContext context = new ApplicationContext())
             {
-                //context.Clients.Add(new Client
-                //{
-                //    FirstName = "QWE",
-                //    LastName = "QWE",
-                //    Patronymic = "QWE"
-                //});
-                //context.SaveChanges();
-
-                //listClients = context.Clients.ToList();
-                //var result = infoClients.Join(listClients,
-                //    i => i.Client_id,
-                //    l => l.ID,
-                //    (i, l) => new { }).ToList();
-
-                //foreach(var client in listClients)
-                //{
-                //    infoClients = context.InfoClients.Where(x => x.Client_id == client.ID).ToList();
-                //}
-                /////////////////////////////////////////////////////////
                 listClients = context.Clients.ToList();
-                //clients = (List<(Client, InfoClient)>)listClients.Join(infoClients,
+                shops = context.ListShops.ToList();
+
+                //var result = listClients.Join(shops,
                 //    c => c.ID,
-                //    i => i.Client_id,
-                //    (c, i) => new { });
+                //    s => s.Client_id,
+                //    (c, s) => new
+                //    {
+                //        FirstName = c.FirstName,
+                //        LastName = c.LastName,
+                //        Patronymic = c.Patronymic,
+                //        DateShop = s.DateShop,
+                //        SumShop = s.SumShop,
+                //        PictureShop = s.PictureShop
+                //    });
             }
 
         }
